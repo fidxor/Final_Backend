d0d11f2e478502de76560a8d9edf2ae4176c12d2
fidxor <fidxordl5404@gmail.com>
2023-09-05 10:38:34 +0900

    Create Backend Project

---
+++ src/test/java/project/lincook/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package project.lincook.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
